98256b95208553a2d673fdf62de25e6be45fc6a2
nahuel <avilanahuel99@gmail.com>
2024-09-07 18:39:52 -0300

    3. Multithread

--- networking/socket/server.py
+++ networking/socket/server.py
@@ -9,4 +9,26 @@ def run_server():
     print(f"Listening on {HOST} in the port {PORT}")
     client_socket, client_address = server.accept()
     print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+
+    #While mantiene servidor en espera de mensajes
+    while True:
+        #recv obtain client response
+        request = client_socket.recv(1024)
+        #Convert byte to string
+        request = request.decode("utf-8")
+
+        if (request.lower() == "close"):
+            client_socket.send("closed".encode("utf-8"))
+            break
+
+        print(f"Received: {request}")
+        response = "accepted".encode("utf-8")
+        client_socket.send(response)
+    
+    client_socket.close()
+    print("Connection closed")
+    server.close()
+
+run_server()
+
     
