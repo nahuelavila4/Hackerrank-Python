6f24e54468d219da05573193beb4c7f964725214
nahuelavila4 <avilanahuel99@gmail.com>
2024-09-08 13:15:41 -0300

    4. Exercise list

--- listas/comprension_listas2.py
+++ listas/comprension_listas2.py
@@ -14,17 +14,36 @@ vocales_cadena = [caract for caract in cadena for elem in vocales if caract.lowe
 
 palabras = ["Hola", "Mundo", "Python"]
 reverso = [palab[::-1] for palab in palabras ]
-print("Palabras al reverso")
-print(reverso)
+#print("Palabras al reverso")
+#print(reverso)
 
 # ---------------------------------------------
-# Ejercicio 10: Crear una lista con el reverso de las palabras
+# Anotaciones Hackerrank - Ejercicio 1
 
+a = 1
+b = 2
+c = 1
+n = 3
 
+valores = []
+combinaciones = [[x, y, z] for x in range(0, a+1) for y in range(0, b+1) for z in range(0, c+1) if (x+y+z) != n]
 
+# ---------------------------------------------
+# Anotaciones Hackerrank - Ejercicio 2
 
+numeros = [1241, 45, 16, 15475, 2640]
+mayor = None
+runner_up = None
 
+for index, val in enumerate(numeros):
+    if mayor == None or val > mayor:
+        runner_up = mayor
+        mayor = val
+    elif runner_up == None or (val < mayor and val > runner_up):
+        runner_up = val
 
+#print("Numero mayor: "+ str(mayor))
+#print("Segundo numero mayor: "+ str(runner_up))
 
 
 
