18ef3115f98260b23f919b63aa4869d6f6f9e2be
nahuelavila4 <avilanahuel99@gmail.com>
2024-12-07 12:08:48 -0300

    15. Save HTTP Status

--- files_handler/find.py
+++ files_handler/find.py
@@ -3,9 +3,15 @@ import os
 def fbn():
     ruta = os.path.dirname(os.path.abspath(__file__))
     ruta_archivo = os.path.join(ruta, "logs_test/webserver.log")
-    with open(ruta_archivo, "r") as f:
-        content = f.read()
-        print(content)
+    with open(ruta_archivo) as f:
+        lista = f.readlines()
+        for i in range(0, len(lista)):
+            s = lista[i].split()
+            status = s[-2]
+            if int(status) != 200:
+                print(f"La peticion {i+1} devolvio un codigo de estado erroneo")
+            else:
+                print(f"La peticion {i+1} devolvio un codigo de estado correcto")
 
 
 fbn()
